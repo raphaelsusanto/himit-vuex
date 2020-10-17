e1ef9091649c2bc3f85cf14402565ff6cef38b95
raphaelsusanto <raphael.susanto@gmail.com>
2020-10-17 16:32:49 +0700

    add transition in router view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <app-navigation-bar></app-navigation-bar>
+    <transition name="slide" mode="out-in">
     <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -21,8 +23,40 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+    }
 
+    .slide-leave-active {
+        transition: opacity 0.2s ease;
+        opacity: 0;
+        animation: slide-out 0.2s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 0.2s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -70,6 +70,7 @@ export default {
   methods: {
     logout() {
       this.$store.dispatch("logout");
+      this.$router.push('/Login');
     },
   },
 };
